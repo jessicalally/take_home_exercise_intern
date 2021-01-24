5237b4b1f6944edc2fc8ce8a0f8d6e8669872917
Jessica Lally <jml19@ic.ac.uk>
2021-01-24 19:10:03 +0000

    Update README and add vehicle check on unit tests

--- src/test/java/com/shutl/controller/QuoteControllerFunctionalTest.java
+++ src/test/java/com/shutl/controller/QuoteControllerFunctionalTest.java
@@ -47,68 +47,70 @@ public class QuoteControllerFunctionalTest {
         return objectMapper.readValue(result.getResponse().getContentAsString(), Quote.class);
     }
 
-    private void checkQuote(Quote quote, String pickupPostcode, String deliveryPostcode, long price) {
+    private void checkQuote(Quote quote, String pickupPostcode, String deliveryPostcode,
+        Vehicle vehicle, long price) {
         assertEquals(quote.getPickupPostcode(), pickupPostcode);
         assertEquals(quote.getDeliveryPostcode(), deliveryPostcode);
+        assertEquals(quote.getVehicle(), vehicle);
         assertEquals(quote.getPrice(), new Long(price));
     }
 
     @Test
     public void testBasicService() throws Exception {
         Quote quote = performPostRequest(defaultQuote);
-        checkQuote(quote, "SW1A1AA", "EC2A3LT", 316);
+        checkQuote(quote, "SW1A1AA", "EC2A3LT", Vehicle.DEFAULT, 316);
     }
 
     @Test
     public void testVariablePricingByDistance() throws Exception {
         Quote quote = performPostRequest(defaultQuote);
-        checkQuote(quote, "SW1A1AA", "EC2A3LT", 316);
+        checkQuote(quote, "SW1A1AA", "EC2A3LT", Vehicle.DEFAULT, 316);
 
         Quote newQuote = new Quote("AL15WD", "EC2A3LT", Vehicle.DEFAULT);
         newQuote = performPostRequest(newQuote);
-        checkQuote(newQuote, "AL15WD", "EC2A3LT", 305);
+        checkQuote(newQuote, "AL15WD", "EC2A3LT", Vehicle.DEFAULT, 305);
     }
 
     @Test
     public void testPricingOfBicycleDelivery() throws Exception {
         defaultQuote.setVehicle(Vehicle.BICYCLE);
         Quote quote = performPostRequest(defaultQuote);
-        checkQuote(quote, "SW1A1AA", "EC2A3LT", 348);
+        checkQuote(quote, "SW1A1AA", "EC2A3LT", Vehicle.BICYCLE, 348);
     }
 
     @Test
     public void testPricingOfMotorbikeDelivery() throws Exception {
         defaultQuote.setVehicle(Vehicle.MOTORBIKE);
         Quote quote = performPostRequest(defaultQuote);
-        checkQuote(quote, "SW1A1AA", "EC2A3LT", 363);
+        checkQuote(quote, "SW1A1AA", "EC2A3LT", Vehicle.MOTORBIKE, 363);
     }
 
     @Test
     public void testPricingOfParcelCarDelivery() throws Exception {
         defaultQuote.setVehicle(Vehicle.PARCEL_CAR);
         Quote quote = performPostRequest(defaultQuote);
-        checkQuote(quote, "SW1A1AA", "EC2A3LT", 379);
+        checkQuote(quote, "SW1A1AA", "EC2A3LT", Vehicle.PARCEL_CAR, 379);
     }
 
     @Test
     public void testPricingOfSmallVanDelivery() throws Exception {
         defaultQuote.setVehicle(Vehicle.SMALL_VAN);
         Quote quote = performPostRequest(defaultQuote);
-        checkQuote(quote, "SW1A1AA", "EC2A3LT", 411);
+        checkQuote(quote, "SW1A1AA", "EC2A3LT", Vehicle.SMALL_VAN, 411);
     }
 
     @Test
     public void testPricingOfLargeVanDelivery() throws Exception {
         defaultQuote.setVehicle(Vehicle.LARGE_VAN);
         Quote quote = performPostRequest(defaultQuote);
-        checkQuote(quote, "SW1A1AA", "EC2A3LT", 442);
+        checkQuote(quote, "SW1A1AA", "EC2A3LT", Vehicle.LARGE_VAN, 442);
     }
 
     @Test
     public void testAcceptsPostcodesContainingWhitespace() throws Exception {
         Quote quoteData = new Quote("SW1A 1AA", "EC2A 3LT", Vehicle.DEFAULT);
         Quote quote = performPostRequest(quoteData);;
-        checkQuote(quote, "SW1A1AA", "EC2A3LT", 316);
+        checkQuote(quote, "SW1A1AA", "EC2A3LT", Vehicle.DEFAULT, 316);
     }
 
     @Test
